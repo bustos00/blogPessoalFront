b4b02a5440ddb607a164e28cc4b5323c67ff45fb
sotie <javacursobustos@gmail.com>
2022-04-05 14:08:46 -0300

    Implementação Put e Delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ getAllTema(): Observable<Tema[]>{
   return this.http.get<Tema[]>('https://blogsotie.herokuapp.com/tema', this.token)
 }
 
+getByIdTema(id: number): Observable<Tema>{
+  return this.http.get<Tema>(`https://blogsotie.herokuapp.com/tema/${id}`, this.token)
+}
+
 postTema(tema: Tema): Observable<Tema>{
   return this.http.post<Tema>('https://blogsotie.herokuapp.com/tema', tema, this.token)
+}
+
+putTema(tema: Tema): Observable<Tema>{
+  return this.http.put<Tema>('https://blogsotie.herokuapp.com/tema', tema, this.token)
+}
+
+deleteTema(id: number){
+  return this.http.delete(`https://blogsotie.herokuapp.com/tema/${id}`, this.token)
 }
 
 }
